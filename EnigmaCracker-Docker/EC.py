b03e4a464819f4f7988ef77b6caf99a34e77ae59
Ardiansyah Putra <104452154+apxcodefun@users.noreply.github.com>
2024-06-23 22:33:43 +0700

    Update EC.py

--- EnigmaCracker-Docker/EC.py
+++ EnigmaCracker-Docker/EC.py
@@ -8,7 +8,10 @@ from dotenv import load_dotenv
 from bip_utils import (
     Bip39MnemonicGenerator,
     Bip39SeedGenerator,
+    Bip32,
     Bip44,
+    Bip84,
+    Bip49,
     Bip44Coins,
     Bip44Changes,
     Bip39WordsNum,
@@ -91,7 +94,7 @@ def bip44_BTC_seed_to_address(seed):
     return bip44_addr_ctx.PublicKey().ToAddress()
 
 
-def check_ETH_balance(address, etherscan_api_key, retries=3, delay=5):
+def check_ETH_balance(address, etherscan_api_key, retries=3, delay=4):
     # Etherscan API endpoint to check the balance of an address
     api_url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={etherscan_api_key}"
 
@@ -120,7 +123,7 @@ def check_ETH_balance(address, etherscan_api_key, retries=3, delay=5):
                 return 0
 
 
-def check_BTC_balance(address, retries=3, delay=5):
+def check_BTC_balance(address, retries=3, delay=4):
     # Check the balance of the address
     for attempt in range(retries):
         try:
